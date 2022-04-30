77491b273b43101050ee919fb7d3463a462edf8e
Naomi Carrigan <nhcarrigan@gmail.com>
2022-04-30 14:28:28 -0700

    feat: add tests

--- src/app/confirm/confirm.component.spec.ts
+++ src/app/confirm/confirm.component.spec.ts
@@ -5,21 +5,29 @@ import { ConfirmComponent } from './confirm.component';
 describe('ConfirmComponent', () => {
   let component: ConfirmComponent;
   let fixture: ComponentFixture<ConfirmComponent>;
+  let compiled: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ConfirmComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+      declarations: [ConfirmComponent],
+    }).compileComponents();
     fixture = TestBed.createComponent(ConfirmComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the view correctly', () => {
+    const title = compiled.querySelector('h1');
+    expect(title.textContent.trim()).toBe('Castle');
+    const buttons = compiled.querySelectorAll('.nes-btn');
+    expect(buttons.length).toBe(1);
+    expect(buttons[0].tagName).toBe('A');
+    expect(buttons[0].textContent.trim()).toBe('Explore the town');
+    expect(buttons[0].getAttribute('routerLink')).toBe('/plaza');
   });
 });
--- src/app/landing/landing.component.spec.ts
+++ src/app/landing/landing.component.spec.ts
@@ -5,21 +5,31 @@ import { LandingComponent } from './landing.component';
 describe('LandingComponent', () => {
   let component: LandingComponent;
   let fixture: ComponentFixture<LandingComponent>;
+  let compiled: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LandingComponent ]
-    })
-    .compileComponents();
-  });
-
-  beforeEach(() => {
+      declarations: [LandingComponent],
+    }).compileComponents();
     fixture = TestBed.createComponent(LandingComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render the view correctly', () => {
+    const title = compiled.querySelector('h1');
+    expect(title.textContent.trim()).toBe('Welcome');
+    const buttons = compiled.querySelectorAll('.nes-btn');
+    expect(buttons.length).toBe(1);
+    expect(buttons[0].tagName).toBe('A');
+    expect(buttons[0].textContent.trim()).toBe(
+      'Follow the map - and the noise'
+    );
+    expect(buttons[0].getAttribute('routerLink')).toBe('/plaza');
   });
 });
