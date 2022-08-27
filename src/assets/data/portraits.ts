691cf0f5400f2f5af5c768861dcfcbbace0a19f0
Naomi Carrigan <nhcarrigan@gmail.com>
2022-08-26 21:11:07 -0700

    feat: new art and game (#533)
    * feat: add new art
    
    * fix: correct file name in previous art
    
    * feat: add new game
    
    * fix: correct new file name oops

--- src/assets/data/portraits.ts
+++ src/assets/data/portraits.ts
@@ -110,9 +110,15 @@ export const portraits: Portrait[] = [
     alt: 'Naomi sitting with her legs extended',
   },
   {
-    fileName: 'tokkipon3.png',
+    fileName: 'tokkipon4.png',
     artist: 'Tokkipon',
     url: 'https://twitter.com/tokkiponn',
     alt: 'Naomi extending her arms as if offering a hug',
+  },
+  {
+    fileName: 'tokkipon5.jpg',
+    artist: 'Tokkipon',
+    url: 'https://twitter.com/tokkiponn',
+    alt: 'Naomi winking with a finger on her lips, wearing Miku-inspired lingerie.',
   },
 ];
