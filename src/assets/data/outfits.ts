2a616c9e65df062cc372723cfb887eec924bf2ce
Naomi Carrigan <nhcarrigan@gmail.com>
2022-08-29 22:19:54 -0700

    feat: load more outfits (:

--- src/assets/data/outfits.ts
+++ src/assets/data/outfits.ts
@@ -15,11 +15,46 @@ export const Outfits: Outfit[] = [
     alt: 'Naomi wearing a black off-the-shoulder sun dress, with her hair let down.',
   },
   {
+    name: 'Casual Wear 2',
+    fileName: 'casual-2.png',
+    description:
+      "Another more casual outfit, though this one more closely matches Naomi's daily wear.",
+    alt: 'Naomi wearing a black tube top with mid-arm sleeves and a pleated black and grey skirt. Her hair is in her typical ponytail.',
+  },
+  {
+    name: 'Purple Top',
+    fileName: 'purple.png',
+    description:
+      "Every wardrobe needs a splash of colour, and with Naomi's favourite being purple this outfit is the perfect addition.",
+    alt: 'Naomi wearing a purple lacy top and a black pleated skirt, with her hair let down.',
+  },
+  {
+    name: 'Sundress',
+    fileName: 'sundress.png',
+    description:
+      'While not technically a sundress, Naomi wears it as if it were.',
+    alt: 'Naomi wearing a short dress with a lacy top, her hair in a ponytail.',
+  },
+  {
     name: 'Formal Wear',
     fileName: 'formal.png',
     description:
       "Naomi's formal wear, reserved for more elegant occasions. You know, dinner parties and what not.",
     alt: 'Naomi wearing a black dress with fishnet sleeves, her hair in twin braids.',
+  },
+  {
+    name: 'Cocktail Dress',
+    fileName: 'cocktail.png',
+    description:
+      'Specifically for fancy dinner parties, Naomi typically opts for her more generic formal wear.',
+    alt: 'Naomi wearing a black and blue strapless dress, skin-tight and ending halfway down her thighs. Her hair is in her typical ponytail.',
+  },
+  {
+    name: 'Professional Attire',
+    fileName: 'professional.png',
+    description:
+      "This isn't her favourite outfit, but Naomi has this one for situations that require a more professional appearance.",
+    alt: 'Naomi wearing a black long-sleeve ankle-length dress with white lace on the hems. ',
   },
   {
     name: 'Sleepwear',
@@ -33,6 +68,19 @@ export const Outfits: Outfit[] = [
     description:
       'Even Naomi enjoys a trip to the beach. She usually wears a shawl though - being a bit sensitive to the sun.',
     alt: 'Naomi wearing a black bikini and a lace shrug. Her hair has been let down.',
+  },
+  {
+    name: 'Moon Dress',
+    fileName: 'moon.png',
+    description:
+      'While not her typical black attire, Naomi saw this and fell in love with the space theme.',
+    alt: 'Naomi wearing a blue/purple dress with off-the-shoulder sleeves, star patterns in the fabric, and a moon-shaped cutout in the chest. Her hair is in her typical ponytail.',
+  },
+  {
+    name: 'Leather Outfit',
+    fileName: 'leather.png',
+    description: 'Every girl needs a leather outfit, right??',
+    alt: 'Naomi wearing a leather bustier with straps and a leather miniskirt. Her hair is in short pigtails.',
   },
   {
     name: 'Maid Outfit',
@@ -63,10 +111,23 @@ export const Outfits: Outfit[] = [
     alt: 'Naomi wearing a black bustier with straps in the shape of a pentagram, a lacy black shawl, and a tight black skirt. Her hair is in thick twin braids.',
   },
   {
+    name: 'Vampire Dress',
+    fileName: 'vampire.png',
+    description:
+      'Another fun costume, Naomi sometimes wears fake fangs with this.',
+    alt: 'Naomi wearing a black strapless dress with a ragged red hem, her hair in a ponytail.',
+  },
+  {
     name: 'Shrine Maiden',
     fileName: 'shrine.png',
     description:
       "Naomi's uniform she wears when tending to the shrine. Probably best not to ask what the shrine is devoted to.",
     alt: 'Naomi wearing a stylised short black and grey kimono with a floral pattern and detatched sleeves. Her hair is in long twin braids.',
+  },
+  {
+    name: 'Tape',
+    fileName: 'tape.png',
+    description: 'Even Naomi likes to have a little fun sometimes!',
+    alt: 'Naomi wearing black body-tape, with her hair in a ponytail.',
   },
 ];
