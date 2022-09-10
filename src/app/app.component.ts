2c730bc2f844f609ba6f3d09c49c313cb69bd7c1
Naomi Carrigan <nhcarrigan@gmail.com>
2022-09-09 18:39:11 -0700

    feat: load particle config

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,7 @@ import {
   Engine,
 } from 'tsparticles-engine';
 import { loadFull } from 'tsparticles';
+import { ParticleOptions } from 'src/assets/config/ParticleOptions';
 
 @Component({
   selector: 'app-root',
@@ -20,78 +21,7 @@ export class AppComponent {
   id = 'tsparticles';
 
   /* or the classic JavaScript object */
-  particlesOptions = {
-    background: {
-      color: {
-        value: '#0d47a1',
-      },
-    },
-    fpsLimit: 120,
-    interactivity: {
-      events: {
-        onClick: {
-          enable: true,
-          mode: ClickMode.push,
-        },
-        onHover: {
-          enable: true,
-          mode: HoverMode.repulse,
-        },
-        resize: true,
-      },
-      modes: {
-        push: {
-          quantity: 4,
-        },
-        repulse: {
-          distance: 200,
-          duration: 0.4,
-        },
-      },
-    },
-    particles: {
-      color: {
-        value: '#ffffff',
-      },
-      links: {
-        color: '#ffffff',
-        distance: 150,
-        enable: true,
-        opacity: 0.5,
-        width: 1,
-      },
-      collisions: {
-        enable: true,
-      },
-      move: {
-        direction: MoveDirection.none,
-        enable: true,
-        outModes: {
-          default: OutMode.bounce,
-        },
-        random: false,
-        speed: 6,
-        straight: false,
-      },
-      number: {
-        density: {
-          enable: true,
-          area: 800,
-        },
-        value: 80,
-      },
-      opacity: {
-        value: 0.5,
-      },
-      shape: {
-        type: 'circle',
-      },
-      size: {
-        value: { min: 1, max: 5 },
-      },
-    },
-    detectRetina: true,
-  };
+  particlesOptions = ParticleOptions;
 
   particlesLoaded(container: Container): void {
     console.log(container);
