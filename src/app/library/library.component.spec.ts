d676cb66002cbead858a7932cdb17d8da1428a00
Naomi Carrigan <nhcarrigan@gmail.com>
2022-09-18 15:00:17 -0700

    feat: overhaul tests

--- src/app/library/library.component.spec.ts
+++ src/app/library/library.component.spec.ts
@@ -1,11 +1,12 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { library } from 'src/assets/data/library';
 
 import { LibraryComponent } from './library.component';
 
 describe('LibraryComponent', () => {
   let component: LibraryComponent;
   let fixture: ComponentFixture<LibraryComponent>;
-  let compiled: any;
+  let compiled: HTMLElement;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -24,38 +25,40 @@ describe('LibraryComponent', () => {
   it('should render the intro view', () => {
     expect(component.openBook).toBeNull();
     const title = compiled.querySelector('h1');
-    expect(title.textContent.trim()).toBe('Library');
+    expect(title?.textContent?.trim()).toBe('Library');
     const buttons = compiled.querySelectorAll('.nes-btn');
     expect(buttons.length).toBe(4);
     expect(buttons[0].tagName).toBe('BUTTON');
-    expect(buttons[0].textContent.trim()).toBe('Who is Naomi?');
+    expect(buttons[0].textContent?.trim()).toBe(library[0].title);
     expect(buttons[1].tagName).toBe('BUTTON');
-    expect(buttons[1].textContent.trim()).toBe('Before The Journey');
+    expect(buttons[1].textContent?.trim()).toBe(library[1].title);
     expect(buttons[2].tagName).toBe('BUTTON');
-    expect(buttons[2].textContent.trim()).toBe('The Life of a Developer');
+    expect(buttons[2].textContent?.trim()).toBe(library[2].title);
     expect(buttons[3].tagName).toBe('A');
-    expect(buttons[3].textContent.trim()).toBe('Thank her, and leave');
+    expect(buttons[3].textContent?.trim()).toBe('Thank her, and leave');
     expect(buttons[3].getAttribute('routerLink')).toBe('/plaza');
   });
 
-  it('should render a book', () => {
-    component.loadBook('naomi');
-    fixture.detectChanges();
-    compiled = fixture.nativeElement;
-    expect(component.openBook?.key).toBe('naomi');
-    const title = compiled.querySelector('h1');
-    expect(title.textContent.trim()).toBe(component.openBook?.title);
-    const contents = compiled.querySelector('.is-dark');
-    const lines = contents.querySelectorAll('p');
-    expect(lines.length).toBe(component.openBook?.text.length);
-    const buttons = compiled.querySelectorAll('.nes-btn');
-    expect(buttons.length).toBe(3);
-    expect(buttons[0].tagName).toBe('BUTTON');
-    expect(buttons[0].textContent.trim()).toBe('Before The Journey');
-    expect(buttons[1].tagName).toBe('BUTTON');
-    expect(buttons[1].textContent.trim()).toBe('The Life of a Developer');
-    expect(buttons[2].tagName).toBe('A');
-    expect(buttons[2].textContent.trim()).toBe('Thank her, and leave');
-    expect(buttons[2].getAttribute('routerLink')).toBe('/plaza');
-  });
+  for (const book of library) {
+    it(`should render the ${book.title} book correctly`, () => {
+      component.loadBook(book.key);
+      fixture.detectChanges();
+      compiled = fixture.nativeElement;
+      expect(component.openBook?.key).toBe(book.key);
+      const title = compiled.querySelector('h1');
+      expect(title?.textContent?.trim()).toBe(book.title);
+      const contents = compiled.querySelector('.is-dark');
+      const lines = contents?.querySelectorAll('p');
+      expect(lines?.length).toBe(book.text.length);
+      const buttons = compiled.querySelectorAll('.nes-btn');
+      expect(buttons.length).toBe(3);
+      expect(buttons[0].tagName).toBe('BUTTON');
+      expect(buttons[0].textContent?.trim()).toBe(library.filter(e => e.key !== book.key)[0].title);
+      expect(buttons[1].tagName).toBe('BUTTON');
+      expect(buttons[1].textContent?.trim()).toBe(library.filter(e => e.key !== book.key)[1].title);
+      expect(buttons[2].tagName).toBe('A');
+      expect(buttons[2].textContent?.trim()).toBe('Thank her, and leave');
+      expect(buttons[2].getAttribute('routerLink')).toBe('/plaza');
+    });
+  }
 });
