ac435c29e8ded3d4dce4023699ea5c81d66082b0
Naomi Carrigan <nhcarrigan@gmail.com>
2022-12-31 20:54:32 -0800

    feat: paginate tattoos

--- src/app/reference/reference.component.ts
+++ src/app/reference/reference.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { tattoos } from 'src/assets/data/tattoos';
 
 @Component({
   selector: 'app-reference',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReferenceComponent implements OnInit {
   public view = '';
+  public tattooData = tattoos;
+  public currentTattooIndex = 0;
 
   constructor() {}
 
@@ -15,5 +18,23 @@ export class ReferenceComponent implements OnInit {
   changeView(name: string) {
     this.view = name;
     window.scrollTo({ top: 0 });
+  }
+
+  nextTattoo() {
+    this.currentTattooIndex =
+      this.currentTattooIndex === this.tattooData.length - 1
+        ? 0
+        : this.currentTattooIndex + 1;
+  }
+
+  previousTattoo() {
+    this.currentTattooIndex =
+      this.currentTattooIndex === 0
+        ? this.tattooData.length - 1
+        : this.currentTattooIndex - 1;
+  }
+
+  selectTattoo(index: string) {
+    this.currentTattooIndex = parseInt(index);
   }
 }
