f3eaaede52a7a3a92821e570c49310616ae84b5e
Naomi Carrigan <nhcarrigan@gmail.com>
2022-12-31 19:17:36 -0800

    feat: redesign museum component

--- src/app/museum/museum.component.ts
+++ src/app/museum/museum.component.ts
@@ -13,6 +13,8 @@ export class MuseumComponent implements OnInit {
   public view = 'intro';
   public portraits: Portrait[] = [];
   public emotes: Emote[] = [];
+  public currentPortraitIndex = 0;
+  public currentEmoteIndex = 0;
 
   constructor() {}
 
@@ -24,5 +26,41 @@ export class MuseumComponent implements OnInit {
   changeView(name: string) {
     this.view = name;
     window.scrollTo({ top: 0 });
+  }
+
+  nextPortrait() {
+    this.currentPortraitIndex =
+      this.currentPortraitIndex === this.portraits.length - 1
+        ? 0
+        : this.currentPortraitIndex + 1;
+  }
+
+  previousPortrait() {
+    this.currentPortraitIndex =
+      this.currentPortraitIndex === 0
+        ? this.portraits.length - 1
+        : this.currentPortraitIndex - 1;
+  }
+
+  selectPortrait(index: string) {
+    this.currentPortraitIndex = parseInt(index);
+  }
+
+  nextEmote() {
+    this.currentEmoteIndex =
+      this.currentEmoteIndex === this.emotes.length - 1
+        ? 0
+        : this.currentEmoteIndex + 1;
+  }
+
+  previousEmote() {
+    this.currentEmoteIndex =
+      this.currentEmoteIndex === 0
+        ? this.emotes.length - 1
+        : this.currentEmoteIndex - 1;
+  }
+
+  selectEmote(index: string) {
+    this.currentEmoteIndex = parseInt(index);
   }
 }
