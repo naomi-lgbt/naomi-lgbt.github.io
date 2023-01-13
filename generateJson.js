040fba3ab0fa491ecf875f1705013a5f1e9eea6f
Naomi Carrigan <nhcarrigan@gmail.com>
2023-01-12 19:43:51 -0800

    feat: load full outfit data into JSON (#585)

--- generateJson.js
+++ generateJson.js
@@ -1,9 +1,12 @@
-const { readdir, writeFile } = require("fs/promises");
+const { readFile, writeFile } = require("fs/promises");
 const { join } = require("path");
+const djson = require('dirty-json');
 
 (async () => {
-    const outfitsPath = join(process.cwd(), "src", "assets", "img", "outfits");
-    const outfitsFiles = await readdir(outfitsPath);
-    const data = JSON.stringify(outfitsFiles);
+    const outfitsPath = join(process.cwd(), "src", "assets", "data", "outfits.ts");
+    const outfitsData = await readFile(outfitsPath, 'utf-8');
+    const cleanedData = outfitsData.replace(`import { Outfit } from 'src/interfaces/Outfit';\n\nexport const Outfits: Outfit[] = `, '').replace(";", '');
+    const parsedData = djson.parse(cleanedData);
+    const data = JSON.stringify(parsedData, null, 2);
     await writeFile(join(process.cwd(), "src", "assets", "data", "outfits.json"), data, "utf-8");
 })();
