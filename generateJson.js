3b0acd8ec505a0c32cf610b46ca2e31803f7f1d5
Naomi Carrigan <nhcarrigan@gmail.com>
2023-01-12 20:41:32 -0800

    feat: add json for other assets

--- generateJson.js
+++ generateJson.js
@@ -3,10 +3,38 @@ const { join } = require("path");
 const djson = require('dirty-json');
 
 (async () => {
+    // Outfits
     const outfitsPath = join(process.cwd(), "src", "assets", "data", "outfits.ts");
     const outfitsData = await readFile(outfitsPath, 'utf-8');
-    const cleanedData = outfitsData.replace(`import { Outfit } from 'src/interfaces/Outfit';\n\nexport const Outfits: Outfit[] = `, '').replace(";", '');
-    const parsedData = djson.parse(cleanedData);
-    const data = JSON.stringify(parsedData, null, 2);
-    await writeFile(join(process.cwd(), "src", "assets", "data", "outfits.json"), data, "utf-8");
+    const cleanedOutfitData = outfitsData.replace(`import { Outfit } from 'src/interfaces/Outfit';\n\nexport const Outfits: Outfit[] = `, '').replace(";", '');
+    const parsedOutfitData = djson.parse(cleanedOutfitData);
+    const outfitData = JSON.stringify(parsedOutfitData, null, 2);
+    await writeFile(join(process.cwd(), "src", "assets", "data", "outfits.json"), outfitData, "utf-8");
+
+    // Adventures
+
+    const adventuresPath = join(process.cwd(), "src", "assets", "data", "adventures.ts");
+    const adventuresData = await readFile(adventuresPath, 'utf-8');
+    const cleanedAdventureData = adventuresData.replace(`import { Adventure } from 'src/interfaces/Adventure';\n\nexport const adventures: Adventure[] = `, '').replace(";", '');
+    const parsedAdventureData = djson.parse(cleanedAdventureData);
+    const adventureData = JSON.stringify(parsedAdventureData, null, 2);
+    await writeFile(join(process.cwd(), "src", "assets", "data", "adventures.json"), adventureData, "utf-8");
+
+    // Art
+
+    const artPath = join(process.cwd(), "src", "assets", "data", "portraits.ts");
+    const artData = await readFile(artPath, 'utf-8');
+    const cleanedArtData = artData.replace(`import { Portrait } from 'src/interfaces/Portrait';\n\nexport const portraits: Portrait[] = `, '').replace(";", '');
+    const parsedArtData = djson.parse(cleanedArtData);
+    const portraitData = JSON.stringify(parsedArtData, null, 2);
+    await writeFile(join(process.cwd(), "src", "assets", "data", "portraits.json"), portraitData, "utf-8");
+
+    // Emotes
+
+    const emotesPath = join(process.cwd(), "src", "assets", "data", "emotes.ts");
+    const emotesData = await readFile(emotesPath, 'utf-8');
+    const cleanedEmotesData = emotesData.replace(`import { Emote } from 'src/interfaces/Emote';\n\nexport const emotes: Emote[] = `, '').replace(";", '');
+    const parsedEmotesData = djson.parse(cleanedEmotesData);
+    const emoteData = JSON.stringify(parsedEmotesData, null, 2);
+    await writeFile(join(process.cwd(), "src", "assets", "data", "emotes.json"), emoteData, "utf-8");
 })();
