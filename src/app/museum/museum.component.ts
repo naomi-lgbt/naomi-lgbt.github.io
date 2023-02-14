dcae01bcf4a4f0290bcc40831ebce2f062be0280
Naomi Carrigan <nhcarrigan@gmail.com>
2023-02-13 18:29:47 -0800

    feat: add murals view

--- src/app/museum/museum.component.ts
+++ src/app/museum/museum.component.ts
@@ -3,6 +3,9 @@ import { emotes } from 'src/app/_data/emotes';
 import { portraits } from 'src/app/_data/portraits';
 import { Emote } from 'src/interfaces/Emote';
 import { Portrait } from 'src/interfaces/Portrait';
+import { Poses } from '../_data/poses';
+
+type viewType = 'intro' | 'portrait' | 'emote' | 'pose';
 
 @Component({
   selector: 'app-museum',
@@ -10,20 +13,23 @@ import { Portrait } from 'src/interfaces/Portrait';
   styleUrls: ['./museum.component.css'],
 })
 export class MuseumComponent implements OnInit {
-  public view = 'intro';
+  public view: viewType = 'intro';
   public portraits: Portrait[] = [];
   public emotes: Emote[] = [];
+  public poses: string[] = [];
   public currentPortraitIndex = 0;
   public currentEmoteIndex = 0;
+  public currentPoseIndex = 0;
 
   constructor() {}
 
   ngOnInit(): void {
     this.portraits = portraits;
     this.emotes = emotes;
+    this.poses = Poses;
   }
 
-  changeView(name: string) {
+  changeView(name: viewType) {
     this.view = name;
     window.scrollTo({ top: 0 });
   }
@@ -62,5 +68,30 @@ export class MuseumComponent implements OnInit {
 
   selectEmote(index: string) {
     this.currentEmoteIndex = parseInt(index);
+  }
+
+  nextPose() {
+    this.currentPoseIndex =
+      this.currentPoseIndex === this.poses.length - 1
+        ? 0
+        : this.currentPoseIndex + 1;
+  }
+
+  previousPose() {
+    this.currentPoseIndex =
+      this.currentPoseIndex === 0
+        ? this.poses.length - 1
+        : this.currentPoseIndex - 1;
+  }
+
+  selectPose(index: string) {
+    this.currentPoseIndex = parseInt(index);
+  }
+
+  getPoseName(fileName: string) {
+    const withoutExtension = fileName.split('.')[0];
+    const [name, number] = withoutExtension.split('-');
+    const titleCasedName = `${name[0].toUpperCase()}${name.slice(1)}`;
+    return number ? `${titleCasedName} ${number}` : titleCasedName;
   }
 }
