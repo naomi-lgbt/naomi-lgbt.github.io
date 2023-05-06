1e6989156f31cbfbd416ba8096dddb5f9bd3c4c7
Naomi Carrigan <nhcarrigan@gmail.com>
2023-05-05 22:51:31 -0700

    fix: tests

--- src/app/museum/museum.component.spec.ts
+++ src/app/museum/museum.component.spec.ts
@@ -1,185 +1,214 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { MuseumComponent } from './museum.component';
-// import { HttpClientModule } from '@angular/common/http';
-// import { AssetsService } from '../assets.service';
+import { MuseumComponent } from './museum.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AssetsService } from '../assets.service';
 
-// describe('MuseumComponent', () => {
-//   let component: MuseumComponent;
-//   let fixture: ComponentFixture<MuseumComponent>;
-//   let compiled: HTMLElement;
+describe('MuseumComponent', () => {
+  let component: MuseumComponent;
+  let fixture: ComponentFixture<MuseumComponent>;
+  let compiled: HTMLElement;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [MuseumComponent],
-//       imports: [HttpClientModule],
-//       providers: [AssetsService],
-//     }).compileComponents();
-//     fixture = TestBed.createComponent(MuseumComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//     compiled = fixture.nativeElement;
-//   });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [MuseumComponent],
+      imports: [HttpClientModule],
+      providers: [AssetsService],
+    }).compileComponents();
+    fixture = TestBed.createComponent(MuseumComponent);
+    component = fixture.componentInstance;
+    component.portraits = [
+      {
+        fileName: 'feito1.png',
+        name: 'Peace And Happiness',
+        artist: 'feito161',
+        url: 'https://twitter.com/feito161',
+        alt: 'Naomi standing and making peace signs with her hands.',
+      },
+      {
+        fileName: 'feito2.png',
+        name: 'Blissful Rest',
+        artist: 'feito161',
+        url: 'https://twitter.com/feito161',
+        alt: 'Naomi laying on her stomach with her legs bent upwards.',
+      },
+    ];
+    component.emotes = [
+      {
+        fileName: 'NaomiAnnoyed.png',
+        name: 'Annoyed',
+      },
+      {
+        fileName: 'NaomiBlock.png',
+        name: 'Block',
+      },
+    ];
+    component.poses = ['alone.png', 'ayleid.png'];
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it('should render the intro view', () => {
-//     expect(component.view).toBe('intro');
-//     const title = compiled.querySelector('h1');
-//     expect(title?.textContent?.trim()).toBe('Museum');
-//     const buttons = compiled.querySelectorAll('.nes-btn');
-//     expect(buttons.length).toBe(4);
-//     expect(buttons[0].tagName).toBe('BUTTON');
-//     expect(buttons[0].textContent?.trim()).toBe('Portrait Exhibit');
-//     expect(buttons[1].tagName).toBe('BUTTON');
-//     expect(buttons[1].textContent?.trim()).toBe('Emotion Exhibit');
-//     expect(buttons[2].tagName).toBe('BUTTON');
-//     expect(buttons[2].textContent?.trim()).toBe('Mural Exhibit');
-//     expect(buttons[3].tagName).toBe('A');
-//     expect(buttons[3].textContent?.trim()).toBe('Enough art for now');
-//     expect(buttons[3].getAttribute('routerLink')).toBe('/plaza');
-//   });
+  it('should render the intro view', () => {
+    expect(component.view).toBe('intro');
+    const title = compiled.querySelector('h1');
+    expect(title?.textContent?.trim()).toBe('Museum');
+    const buttons = compiled.querySelectorAll('.nes-btn');
+    expect(buttons.length).toBe(4);
+    expect(buttons[0].tagName).toBe('BUTTON');
+    expect(buttons[0].textContent?.trim()).toBe('Portrait Exhibit');
+    expect(buttons[1].tagName).toBe('BUTTON');
+    expect(buttons[1].textContent?.trim()).toBe('Emotion Exhibit');
+    expect(buttons[2].tagName).toBe('BUTTON');
+    expect(buttons[2].textContent?.trim()).toBe('Mural Exhibit');
+    expect(buttons[3].tagName).toBe('A');
+    expect(buttons[3].textContent?.trim()).toBe('Enough art for now');
+    expect(buttons[3].getAttribute('routerLink')).toBe('/plaza');
+  });
 
-//   it('should render the portrait view', () => {
-//     component.changeView('portrait');
-//     fixture.detectChanges();
-//     compiled = fixture.nativeElement;
-//     expect(component.view).toBe('portrait');
-//     const title = compiled.querySelector('h1');
-//     expect(title?.textContent?.trim()).toBe('Museum');
-//     const buttons = compiled.querySelectorAll('.nes-btn');
-//     expect(buttons.length).toBe(5);
-//     expect(buttons[0].tagName).toBe('BUTTON');
-//     expect(buttons[0].textContent?.trim()).toBe('Previous Portrait');
-//     expect(buttons[1].tagName).toBe('BUTTON');
-//     expect(buttons[1].textContent?.trim()).toBe('Next Portrait');
-//     expect(buttons[2].tagName).toBe('BUTTON');
-//     expect(buttons[2].textContent?.trim()).toBe('Emotion Exhibit');
-//     expect(buttons[3].tagName).toBe('BUTTON');
-//     expect(buttons[3].textContent?.trim()).toBe('Mural Exhibit');
-//     expect(buttons[4].tagName).toBe('A');
-//     expect(buttons[4].textContent?.trim()).toBe('Enough art for now');
-//     expect(buttons[4].getAttribute('routerLink')).toBe('/plaza');
-//   });
+  it('should render the portrait view', () => {
+    component.changeView('portrait');
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
+    expect(component.view).toBe('portrait');
+    const title = compiled.querySelector('h1');
+    expect(title?.textContent?.trim()).toBe('Museum');
+    const buttons = compiled.querySelectorAll('.nes-btn');
+    expect(buttons.length).toBe(5);
+    expect(buttons[0].tagName).toBe('BUTTON');
+    expect(buttons[0].textContent?.trim()).toBe('Previous Portrait');
+    expect(buttons[1].tagName).toBe('BUTTON');
+    expect(buttons[1].textContent?.trim()).toBe('Next Portrait');
+    expect(buttons[2].tagName).toBe('BUTTON');
+    expect(buttons[2].textContent?.trim()).toBe('Emotion Exhibit');
+    expect(buttons[3].tagName).toBe('BUTTON');
+    expect(buttons[3].textContent?.trim()).toBe('Mural Exhibit');
+    expect(buttons[4].tagName).toBe('A');
+    expect(buttons[4].textContent?.trim()).toBe('Enough art for now');
+    expect(buttons[4].getAttribute('routerLink')).toBe('/plaza');
+  });
 
-//   it(`should render the portraits correctly`, () => {
-//     for (const portrait of component.portraits) {
-//       component.changeView('portrait');
-//       component.selectPortrait(String(component.portraits.indexOf(portrait)));
-//       fixture.detectChanges();
-//       compiled = fixture.nativeElement;
-//       expect(component.view).toBe('portrait');
-//       const portraitBox = compiled.querySelector('.image');
-//       const links = portraitBox?.querySelectorAll('a');
-//       const imageLink = links?.[0];
-//       expect(imageLink?.getAttribute('href')).toBe(
-//         `https://cdn.naomi.lgbt/naomi/art/${portrait.fileName}`
-//       );
-//       expect(imageLink?.getAttribute('target')).toBe('_blank');
-//       const artistLink = links?.[1];
-//       expect(artistLink?.getAttribute('href')).toBe(portrait.url);
-//       expect(artistLink?.getAttribute('target')).toBe('_blank');
-//       const img = portraitBox?.querySelector('img');
-//       expect(img?.getAttribute('src')).toBe(
-//         `https://cdn.naomi.lgbt/naomi/art/${portrait.fileName}`
-//       );
-//       expect(img?.getAttribute('alt')).toBe(portrait.alt);
-//       const title = portraitBox?.querySelector('p');
-//       expect(title?.textContent?.trim()).toBe(`By ${portrait.artist}`);
-//     }
-//   });
+  it(`should render the portraits correctly`, () => {
+    for (const portrait of component.portraits) {
+      component.changeView('portrait');
+      component.selectPortrait(String(component.portraits.indexOf(portrait)));
+      fixture.detectChanges();
+      compiled = fixture.nativeElement;
+      expect(component.view).toBe('portrait');
+      const portraitBox = compiled.querySelector('.image');
+      const links = portraitBox?.querySelectorAll('a');
+      const imageLink = links?.[0];
+      expect(imageLink?.getAttribute('href')).toBe(
+        `https://cdn.naomi.lgbt/naomi/art/${portrait.fileName}`
+      );
+      expect(imageLink?.getAttribute('target')).toBe('_blank');
+      const artistLink = links?.[1];
+      expect(artistLink?.getAttribute('href')).toBe(portrait.url);
+      expect(artistLink?.getAttribute('target')).toBe('_blank');
+      const img = portraitBox?.querySelector('img');
+      expect(img?.getAttribute('src')).toBe(
+        `https://cdn.naomi.lgbt/naomi/art/${portrait.fileName}`
+      );
+      expect(img?.getAttribute('alt')).toBe(portrait.alt);
+      const title = portraitBox?.querySelector('p');
+      expect(title?.textContent?.trim()).toBe(
+        `${portrait.name} By ${portrait.artist}`
+      );
+    }
+  });
 
-//   it('should render the emote view', () => {
-//     component.changeView('emote');
-//     fixture.detectChanges();
-//     compiled = fixture.nativeElement;
-//     expect(component.view).toBe('emote');
-//     const title = compiled.querySelector('h1');
-//     expect(title?.textContent?.trim()).toBe('Museum');
-//     const buttons = compiled.querySelectorAll('.nes-btn');
-//     expect(buttons.length).toBe(5);
-//     expect(buttons[0].tagName).toBe('BUTTON');
-//     expect(buttons[0].textContent?.trim()).toBe('Previous Emote');
-//     expect(buttons[1].tagName).toBe('BUTTON');
-//     expect(buttons[1].textContent?.trim()).toBe('Next Emote');
-//     expect(buttons[2].tagName).toBe('BUTTON');
-//     expect(buttons[2].textContent?.trim()).toBe('Portrait Exhibit');
-//     expect(buttons[3].tagName).toBe('BUTTON');
-//     expect(buttons[3].textContent?.trim()).toBe('Mural Exhibit');
-//     expect(buttons[4].tagName).toBe('A');
-//     expect(buttons[4].textContent?.trim()).toBe('Enough art for now');
-//     expect(buttons[4].getAttribute('routerLink')).toBe('/plaza');
-//   });
+  it('should render the emote view', () => {
+    component.changeView('emote');
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
+    expect(component.view).toBe('emote');
+    const title = compiled.querySelector('h1');
+    expect(title?.textContent?.trim()).toBe('Museum');
+    const buttons = compiled.querySelectorAll('.nes-btn');
+    expect(buttons.length).toBe(5);
+    expect(buttons[0].tagName).toBe('BUTTON');
+    expect(buttons[0].textContent?.trim()).toBe('Previous Emote');
+    expect(buttons[1].tagName).toBe('BUTTON');
+    expect(buttons[1].textContent?.trim()).toBe('Next Emote');
+    expect(buttons[2].tagName).toBe('BUTTON');
+    expect(buttons[2].textContent?.trim()).toBe('Portrait Exhibit');
+    expect(buttons[3].tagName).toBe('BUTTON');
+    expect(buttons[3].textContent?.trim()).toBe('Mural Exhibit');
+    expect(buttons[4].tagName).toBe('A');
+    expect(buttons[4].textContent?.trim()).toBe('Enough art for now');
+    expect(buttons[4].getAttribute('routerLink')).toBe('/plaza');
+  });
 
-//   it(`should render the emotes correctly`, () => {
-//     for (const emote of component.emotes) {
-//       component.changeView('emote');
-//       component.selectEmote(String(component.emotes.indexOf(emote)));
-//       fixture.detectChanges();
-//       compiled = fixture.nativeElement;
-//       expect(component.view).toBe('emote');
-//       const emoteBox = compiled.querySelector('.image');
-//       const imageLink = emoteBox?.querySelector('a');
-//       expect(imageLink?.getAttribute('href')).toBe(
-//         `https://cdn.naomi.lgbt/naomi/emotes/${emote.fileName}`
-//       );
-//       expect(imageLink?.getAttribute('target')).toBe('_blank');
-//       const img = emoteBox?.querySelector('img');
-//       expect(img?.getAttribute('src')).toBe(
-//         `https://cdn.naomi.lgbt/naomi/emotes/${emote.fileName}`
-//       );
-//       expect(img?.getAttribute('alt')).toBe('Naomi');
-//       const title = emoteBox?.querySelector('p');
-//       expect(title?.textContent?.trim()).toBe(emote.name);
-//     }
-//   });
+  it(`should render the emotes correctly`, () => {
+    for (const emote of component.emotes) {
+      component.changeView('emote');
+      component.selectEmote(String(component.emotes.indexOf(emote)));
+      fixture.detectChanges();
+      compiled = fixture.nativeElement;
+      expect(component.view).toBe('emote');
+      const emoteBox = compiled.querySelector('.image');
+      const imageLink = emoteBox?.querySelector('a');
+      expect(imageLink?.getAttribute('href')).toBe(
+        `https://cdn.naomi.lgbt/naomi/emotes/${emote.fileName}`
+      );
+      expect(imageLink?.getAttribute('target')).toBe('_blank');
+      const img = emoteBox?.querySelector('img');
+      expect(img?.getAttribute('src')).toBe(
+        `https://cdn.naomi.lgbt/naomi/emotes/${emote.fileName}`
+      );
+      expect(img?.getAttribute('alt')).toBe('Naomi');
+      const title = emoteBox?.querySelector('p');
+      expect(title?.textContent?.trim()).toBe(emote.name);
+    }
+  });
 
-//   it('should render the murals view', () => {
-//     component.changeView('pose');
-//     fixture.detectChanges();
-//     compiled = fixture.nativeElement;
-//     expect(component.view).toBe('pose');
-//     const title = compiled.querySelector('h1');
-//     expect(title?.textContent?.trim()).toBe('Museum');
-//     const buttons = compiled.querySelectorAll('.nes-btn');
-//     expect(buttons.length).toBe(5);
-//     expect(buttons[0].tagName).toBe('BUTTON');
-//     expect(buttons[0].textContent?.trim()).toBe('Previous Mural');
-//     expect(buttons[1].tagName).toBe('BUTTON');
-//     expect(buttons[1].textContent?.trim()).toBe('Next Mural');
-//     expect(buttons[2].tagName).toBe('BUTTON');
-//     expect(buttons[2].textContent?.trim()).toBe('Portrait Exhibit');
-//     expect(buttons[3].tagName).toBe('BUTTON');
-//     expect(buttons[3].textContent?.trim()).toBe('Emotion Exhibit');
-//     expect(buttons[4].tagName).toBe('A');
-//     expect(buttons[4].textContent?.trim()).toBe('Enough art for now');
-//     expect(buttons[4].getAttribute('routerLink')).toBe('/plaza');
-//   });
+  it('should render the murals view', () => {
+    component.changeView('pose');
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
+    expect(component.view).toBe('pose');
+    const title = compiled.querySelector('h1');
+    expect(title?.textContent?.trim()).toBe('Museum');
+    const buttons = compiled.querySelectorAll('.nes-btn');
+    expect(buttons.length).toBe(5);
+    expect(buttons[0].tagName).toBe('BUTTON');
+    expect(buttons[0].textContent?.trim()).toBe('Previous Mural');
+    expect(buttons[1].tagName).toBe('BUTTON');
+    expect(buttons[1].textContent?.trim()).toBe('Next Mural');
+    expect(buttons[2].tagName).toBe('BUTTON');
+    expect(buttons[2].textContent?.trim()).toBe('Portrait Exhibit');
+    expect(buttons[3].tagName).toBe('BUTTON');
+    expect(buttons[3].textContent?.trim()).toBe('Emotion Exhibit');
+    expect(buttons[4].tagName).toBe('A');
+    expect(buttons[4].textContent?.trim()).toBe('Enough art for now');
+    expect(buttons[4].getAttribute('routerLink')).toBe('/plaza');
+  });
 
-//   it(`should render the poses correctly`, () => {
-//     for (const pose of component.poses) {
-//       component.changeView('pose');
-//       component.selectPose(String(component.poses.indexOf(pose)));
-//       fixture.detectChanges();
-//       compiled = fixture.nativeElement;
-//       expect(component.view).toBe('pose');
-//       const emoteBox = compiled.querySelector('.image');
-//       const imageLink = emoteBox?.querySelector('a');
-//       expect(imageLink?.getAttribute('href')).toBe(
-//         `https://cdn.naomi.lgbt/naomi/koikatsu/${pose}`
-//       );
-//       expect(imageLink?.getAttribute('target')).toBe('_blank');
-//       const img = emoteBox?.querySelector('img');
-//       expect(img?.getAttribute('src')).toBe(
-//         `https://cdn.naomi.lgbt/naomi/koikatsu/${pose}`
-//       );
-//       expect(img?.getAttribute('alt')).toBe('Naomi');
-//       const title = emoteBox?.querySelector('p');
-//       expect(title?.textContent?.trim()).toBe(
-//         component.getPoseName(component.poses.indexOf(pose))
-//       );
-//     }
-//   });
-// });
+  it(`should render the poses correctly`, () => {
+    for (const pose of component.poses) {
+      component.changeView('pose');
+      component.selectPose(String(component.poses.indexOf(pose)));
+      fixture.detectChanges();
+      compiled = fixture.nativeElement;
+      expect(component.view).toBe('pose');
+      const emoteBox = compiled.querySelector('.image');
+      const imageLink = emoteBox?.querySelector('a');
+      expect(imageLink?.getAttribute('href')).toBe(
+        `https://cdn.naomi.lgbt/naomi/koikatsu/${pose}`
+      );
+      expect(imageLink?.getAttribute('target')).toBe('_blank');
+      const img = emoteBox?.querySelector('img');
+      expect(img?.getAttribute('src')).toBe(
+        `https://cdn.naomi.lgbt/naomi/koikatsu/${pose}`
+      );
+      expect(img?.getAttribute('alt')).toBe('Naomi');
+      const title = emoteBox?.querySelector('p');
+      expect(title?.textContent?.trim()).toBe(
+        component.getPoseName(component.poses.indexOf(pose))
+      );
+    }
+  });
+});
--- src/app/tailor/tailor.component.spec.ts
+++ src/app/tailor/tailor.component.spec.ts
@@ -1,100 +1,122 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
-// import { TailorComponent } from './tailor.component';
-// import { HttpClientModule } from '@angular/common/http';
-// import { AssetsService } from '../assets.service';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TailorComponent } from './tailor.component';
+import { HttpClientModule } from '@angular/common/http';
+import { AssetsService } from '../assets.service';
 
-// describe('TailorComponent', () => {
-//   let component: TailorComponent;
-//   let fixture: ComponentFixture<TailorComponent>;
-//   let compiled: HTMLElement;
+describe('TailorComponent', () => {
+  let component: TailorComponent;
+  let fixture: ComponentFixture<TailorComponent>;
+  let compiled: HTMLElement;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [TailorComponent],
-//       imports: [HttpClientModule],
-//       providers: [AssetsService],
-//     }).compileComponents();
-//     fixture = TestBed.createComponent(TailorComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//     compiled = fixture.nativeElement;
-//   });
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [TailorComponent],
+      imports: [HttpClientModule],
+      providers: [AssetsService],
+    }).compileComponents();
+    fixture = TestBed.createComponent(TailorComponent);
+    component = fixture.componentInstance;
+    component.outfits = [
+      {
+        name: 'Abyss',
+        fileName: 'abyss.png',
+        description:
+          'Stare long into the abyss and the abyss will stare back into you.',
+        alt: 'Naomi wearing a purple shirt with a strange pattern and a red plaid miniskirt.',
+        credits: {
+          top: 'https://booth.pm/en/items/4608652',
+          skirt: 'https://booth.pm/en/items/3728335',
+        },
+      },
+      {
+        name: 'Aikido Gi',
+        fileName: 'aikido.png',
+        description: 'Naomi is ready to learn the sword.',
+        alt: 'Naomi wearing a white kimono top with a blue hakama.',
+        credits: {
+          outfit: 'https://mohsu.booth.pm/items/4359515',
+        },
+      },
+    ];
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
 
-//   it('should render the view correctly', () => {
-//     const title = compiled.querySelector('h1');
-//     expect(title?.textContent?.trim()).toBe('Tailor');
-//     const buttons = compiled.querySelectorAll('.nes-btn');
-//     expect(buttons[0].tagName).toBe('BUTTON');
-//     expect(buttons[0].textContent?.trim()).toBe('Model Credits');
-//     expect(buttons[buttons.length - 1].tagName).toBe('A');
-//     expect(buttons[buttons.length - 1].textContent?.trim()).toBe(
-//       'These are nice - but time to move on'
-//     );
-//     expect(buttons[buttons.length - 1].getAttribute('routerLink')).toBe(
-//       '/plaza'
-//     );
-//   });
+  it('should render the view correctly', () => {
+    const title = compiled.querySelector('h1');
+    expect(title?.textContent?.trim()).toBe('Tailor');
+    const buttons = compiled.querySelectorAll('.nes-btn');
+    expect(buttons[0].tagName).toBe('BUTTON');
+    expect(buttons[0].textContent?.trim()).toBe('Model Credits');
+    expect(buttons[buttons.length - 1].tagName).toBe('A');
+    expect(buttons[buttons.length - 1].textContent?.trim()).toBe(
+      'These are nice - but time to move on'
+    );
+    expect(buttons[buttons.length - 1].getAttribute('routerLink')).toBe(
+      '/plaza'
+    );
+  });
 
-//   it('should render the model credits correctly', () => {
-//     component.renderCredits('_default');
-//     fixture.detectChanges();
-//     const modal = compiled.querySelector('.modal');
-//     expect(modal).toBeDefined();
-//     const textElements = modal?.querySelectorAll('a');
-//     textElements?.forEach((el, i) => {
-//       expect(el.textContent?.trim()).toBe(component.defaultCredits[i][0]);
-//       expect(el.getAttribute('href')).toBe(component.defaultCredits[i][1]);
-//     });
-//     component.closeModal();
-//     fixture.detectChanges();
-//     const hiddenModal = compiled.querySelector('.modal');
-//     expect(hiddenModal).toBeNull();
-//   });
+  it('should render the model credits correctly', () => {
+    component.renderCredits('_default');
+    fixture.detectChanges();
+    const modal = compiled.querySelector('.modal');
+    expect(modal).toBeDefined();
+    const textElements = modal?.querySelectorAll('a');
+    textElements?.forEach((el, i) => {
+      expect(el.textContent?.trim()).toBe(component.defaultCredits[i][0]);
+      expect(el.getAttribute('href')).toBe(component.defaultCredits[i][1]);
+    });
+    component.closeModal();
+    fixture.detectChanges();
+    const hiddenModal = compiled.querySelector('.modal');
+    expect(hiddenModal).toBeNull();
+  });
 
-//   it(`should render the outfits correctly`, () => {
-//     for (const outfit of component.outfits) {
-//       component.selectOutfit(String(component.outfits.indexOf(outfit)));
-//       fixture.detectChanges();
-//       const outfitBox = compiled.querySelector('.outfit');
-//       const imgLink = outfitBox?.querySelector('a');
-//       expect(imgLink?.getAttribute('href')).toBe(
-//         `https://cdn.naomi.lgbt/naomi/outfits/${outfit.fileName}`
-//       );
-//       expect(imgLink?.getAttribute('target')).toBe('_blank');
-//       const img = outfitBox?.querySelector('img');
-//       expect(img?.getAttribute('src')).toBe(
-//         `https://cdn.naomi.lgbt/naomi/outfits/${outfit.fileName}`
-//       );
-//       expect(img?.getAttribute('alt')).toBe(outfit.alt);
-//       const title = outfitBox?.querySelector('h2');
-//       expect(title?.textContent?.trim()).toBe(outfit.name);
-//       const description = outfitBox?.querySelector('p');
-//       expect(description?.textContent?.trim()).toBe(outfit.description);
-//       const creditButton = outfitBox?.querySelector('.nes-btn');
-//       expect(creditButton?.textContent?.trim()).toBe('Credits');
-//       component.renderCredits(outfit.name);
-//       fixture.detectChanges();
-//       const modal = compiled.querySelector('.modal');
-//       expect(modal).toBeDefined();
-//       const textElements = modal?.querySelectorAll('a');
-//       const textEntries = Object.entries(outfit.credits);
-//       if (!textEntries.length) {
-//         expect(textElements?.[0].textContent?.trim()).toBe(
-//           "We're sorry, but we're still hard at work tracking down the credits for this. Make a PR?"
-//         );
-//         expect(textElements?.[0]?.getAttribute('href')).toBe(
-//           'https://github.com/naomi-lgbt/naomi-lgbt.github.io/pulls'
-//         );
-//       } else {
-//         textElements?.forEach((el, i) => {
-//           expect(el.textContent?.trim()).toBe(textEntries[i][0]);
-//           expect(el.getAttribute('href')).toBe(textEntries[i][1]);
-//         });
-//       }
-//     }
-//   });
-// });
+  it(`should render the outfits correctly`, () => {
+    for (const outfit of component.outfits) {
+      component.selectOutfit(String(component.outfits.indexOf(outfit)));
+      fixture.detectChanges();
+      const outfitBox = compiled.querySelector('.outfit');
+      const imgLink = outfitBox?.querySelector('a');
+      expect(imgLink?.getAttribute('href')).toBe(
+        `https://cdn.naomi.lgbt/naomi/outfits/${outfit.fileName}`
+      );
+      expect(imgLink?.getAttribute('target')).toBe('_blank');
+      const img = outfitBox?.querySelector('img');
+      expect(img?.getAttribute('src')).toBe(
+        `https://cdn.naomi.lgbt/naomi/outfits/${outfit.fileName}`
+      );
+      expect(img?.getAttribute('alt')).toBe(outfit.alt);
+      const title = outfitBox?.querySelector('h2');
+      expect(title?.textContent?.trim()).toBe(outfit.name);
+      const description = outfitBox?.querySelector('p');
+      expect(description?.textContent?.trim()).toBe(outfit.description);
+      const creditButton = outfitBox?.querySelector('.nes-btn');
+      expect(creditButton?.textContent?.trim()).toBe('Credits');
+      component.renderCredits(outfit.name);
+      fixture.detectChanges();
+      const modal = compiled.querySelector('.modal');
+      expect(modal).toBeDefined();
+      const textElements = modal?.querySelectorAll('a');
+      const textEntries = Object.entries(outfit.credits);
+      if (!textEntries.length) {
+        expect(textElements?.[0].textContent?.trim()).toBe(
+          "We're sorry, but we're still hard at work tracking down the credits for this. Make a PR?"
+        );
+        expect(textElements?.[0]?.getAttribute('href')).toBe(
+          'https://github.com/naomi-lgbt/naomi-lgbt.github.io/pulls'
+        );
+      } else {
+        textElements?.forEach((el, i) => {
+          expect(el.textContent?.trim()).toBe(textEntries[i][0]);
+          expect(el.getAttribute('href')).toBe(textEntries[i][1]);
+        });
+      }
+    }
+  });
+});
