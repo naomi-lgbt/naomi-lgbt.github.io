bcacdf797bec5a712566638add1090fdc074f649
Naomi Carrigan <nhcarrigan@gmail.com>
2023-05-08 17:45:22 -0700

    feat: use new asset layout

--- src/interfaces/Assets.ts
+++ src/interfaces/Assets.ts
@@ -2,11 +2,12 @@ import { Adventure } from './Adventure';
 import { Emote } from './Emote';
 import { Outfit } from './Outfit';
 import { Portrait } from './Portrait';
+import { Pose } from './Pose';
 
 export interface Assets {
   adventures: Adventure[];
   emotes: Emote[];
   outfits: Outfit[];
   portraits: Portrait[];
-  poses: string[];
+  poses: Pose[];
 }
