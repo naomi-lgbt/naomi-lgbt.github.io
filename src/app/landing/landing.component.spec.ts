44c15bc1a045920dc404ba5929b590b4e957dd90
Naomi Carrigan <nhcarrigan@gmail.com>
2023-06-29 16:21:24 -0700

    feat: add website explanation to landing page

--- src/app/landing/landing.component.spec.ts
+++ src/app/landing/landing.component.spec.ts
@@ -23,13 +23,14 @@ describe('LandingComponent', () => {
 
   it('should render the view correctly', () => {
     const title = compiled.querySelector('h1');
-    expect(title?.textContent?.trim()).toBe('Welcome');
+    expect(title?.textContent?.trim()).toBe('Naomi Carrigan');
     const buttons = compiled.querySelectorAll('.nes-btn');
-    expect(buttons.length).toBe(1);
+    expect(buttons.length).toBe(2);
     expect(buttons[0].tagName).toBe('A');
-    expect(buttons[0].textContent?.trim()).toBe(
-      'Follow the map - and the noise'
-    );
-    expect(buttons[0].getAttribute('routerLink')).toBe('/plaza');
+    expect(buttons[0].textContent?.trim()).toBe("See Naomi's work");
+    expect(buttons[0].getAttribute('href')).toBe('https://www.nhcarrigan.com');
+    expect(buttons[1].tagName).toBe('A');
+    expect(buttons[1].textContent?.trim()).toBe('Explore this site');
+    expect(buttons[1].getAttribute('routerLink')).toBe('/approach');
   });
 });
