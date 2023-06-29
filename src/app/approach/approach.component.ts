b9cc71a432871e999e69f23853a7262b60e709a7
Naomi Carrigan <nhcarrigan@gmail.com>
2023-06-29 16:30:02 -0700

    chore: remove empty css file

--- src/app/approach/approach.component.ts
+++ src/app/approach/approach.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-approach',
   templateUrl: './approach.component.html',
-  styleUrls: ['./approach.component.css'],
 })
 export class ApproachComponent {}
